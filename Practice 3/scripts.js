3f04db9ac92ae4f3352c35a9727ef1218233681d
ShahnamUlHassan <ibrahim2baig786@gmail.com>
2025-01-30 18:57:20 +0500

    Add files via upload

---
+++ Practice 3/scripts.js
@@ -0,0 +1,21 @@
+// nn bb ss u
+
+let a = "null";
+let b = "345";
+let c =  "Any Value";
+let d = BigInt ("567") + BigInt ("3");
+let e = "Shahnam";
+let f = Symbol("i'm a nice symbol");
+let g = undefined;
+
+console.log(a , b , c , d , e , f , g);
+
+console.log(typeof d);
+
+const item = {
+    "Shahnam":true,
+    "Moon":false,
+    "Hassan":67,
+    "Hello":undefined
+}
+console.log(item["Shahnam"]);
