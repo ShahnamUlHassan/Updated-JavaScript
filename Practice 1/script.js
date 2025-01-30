23ac451cdce9c32abaa50821985e2fac29a74cca
ShahnamUlHassan <ibrahim2baig786@gmail.com>
2025-01-30 18:35:27 +0500

    Add files via upload

---
+++ Practice 1/script.js
@@ -0,0 +1,5 @@
+console.log("Hello World!");
+var a = 67;
+console.log(a);
+a = "Shahnam";
+console.log(a);
---
+++ Practice 2/script.js
@@ -0,0 +1,16 @@
+var a = "Hello World!";
+var b = 67;
+var c = "Moon";
+
+let d = "Hello World";
+let e = 67;
+let f = "Moon";
+
+const g = "Shahnam ul Hassan";
+
+{
+    let b = "Hello";
+    console.log(b)
+}
+console.log(b);
+console.log(g);
