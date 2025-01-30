1e1a83d8dfe74d2230561e255715d9b1ca891a50
ShahnamUlHassan <ibrahim2baig786@gmail.com>
2025-01-30 19:04:16 +0500

    Add files via upload

---
+++ Practice 4/script.js
@@ -0,0 +1,21 @@
+let a = "Shahnam";
+let b = 6;
+
+console.log(a + b);
+
+console.log(typeof(a+b));
+
+const a1 ={
+    name:"Shahnam",
+    section:1,
+    age:26
+}
+a1["friend"] = "Moon";
+console.log(a1);
+
+const dict = {
+    appreciate : "recognize the full worth of",
+    ataraxia : "a state of freedom from emotional disturbance and anxiety",
+    yakka : "work , especially hard work"
+}
+console.log(dict["yakka"]);
