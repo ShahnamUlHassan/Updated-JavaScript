cf80ad8a70e93bcbc1a1cf464bbd4aecbc176870
ShahnamUlHassan <ibrahim2baig786@gmail.com>
2025-02-01 13:08:55 +0500

    Add files via upload

---
+++ Practice 5/script.js
@@ -0,0 +1,35 @@
+console.log("operators in js");
+let a = 45;
+let b = 5;
+
+console.log("a+b=",a+b);
+console.log("a-b=",a-b);
+console.log("a/b=",a/b);
+console.log("a*b=",a*b);
+console.log("a**b=",a**b);
+console.log("a%b=",a%b);
+console.log("a++ = ",a++);
+console.log(a);
+console.log("a-- = ",a--);
+console.log(a);
+console.log("--a = ",--a);
+console.log("++a = ",++a);
+
+let c = 5;
+let d = 5;
+console.log("c == d", c == d);
+console.log("c != d", c != d);
+console.log("c === d", c === d);
+console.log("c !== d", c !== d);
+
+let e = 5;
+let f = "5";
+
+console.log("e === f", e === f);
+console.log("e !== f", e !== f);
+
+let g = 5;
+let h = 5;
+console.log("g > h", g > h);
+console.log(g < h && g == h);
+console.log(g < h || g == h);
