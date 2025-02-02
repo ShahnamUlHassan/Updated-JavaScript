7cefe245692e684d5c451454d109541271464b8d
ShahnamUlHassan <ibrahim2baig786@gmail.com>
2025-02-02 17:05:37 +0500

    Add files via upload

---
+++ Practice 9/script.js
@@ -0,0 +1,18 @@
+// let n = prompt("Enter the value of n");
+// n = Number.parseInt(n)
+
+// let i = 0;
+// while(i<5){
+//     console.log(i)
+//     i++;
+// }
+
+let n = prompt("Enter the value of n");
+n = Number.parseInt(n)
+
+let i = 7;
+do{
+    console.log(i)
+    i++;
+}
+while(i<6);
