76d5d43bc93dd35b356b084d164d8f9d8687efd4
ShahnamUlHassan <ibrahim2baig786@gmail.com>
2025-02-02 20:54:25 +0500

    Add files via upload

---
+++ Practice 11/script.js
@@ -0,0 +1,25 @@
+// let marks = {
+//     Shahnam: 58,
+//     Hassan: 57,
+//     Moon:59
+// }
+// // for(let i=0;i<Object.keys(marks).length;i++){
+// //     console.log("The marks of " + Object.keys(marks)[i] + " are " + marks[Object.keys(marks)[i]])
+// // }
+
+// for (let key in marks){
+//     console.log("The marks of " + key + " are " + marks[key])
+// }
+
+// let cn = 4;
+// let i
+// while(i != cn){
+//     i = prompt("Enter a number")
+//     console.log("Try again")
+// }
+// console.log("You have entered a correct number");
+
+const mean = (a, b, c, d) =>{
+    return(a + b + c + d) / 4
+}
+console.log(mean(4, 5, 6, 7))
