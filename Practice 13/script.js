cd6203b9bf4f00f0920341a563b438230a379259
ShahnamUlHassan <ibrahim2baig786@gmail.com>
2025-02-03 19:37:16 +0500

    Add files via upload

---
+++ Practice 13/script.js
@@ -0,0 +1,26 @@
+// let name = "Shahnam\"";
+// console.log(name.length);
+
+// let name = "Shahnam";
+// console.log(name.toUpperCase());
+// console.log(name.toLowerCase());
+// console.log(name.slice(3,7));
+// console.log(name.slice(2));
+// console.log(name.replace("Shah", "i"));
+
+// let name = "Shahnam";
+// let friend = "Hassan";
+// console.log(name.concat (" is a friend of ", friend))
+
+// let friend2 = "          Risha              ";
+// console.log(friend2);
+// console.log(friend2.trim());
+
+let fr = "Shahnam";
+console.log(fr[0]);
+console.log(fr[1]);
+console.log(fr[2]);
+console.log(fr[3]);
+console.log(fr[4]);
+console.log(fr[5]);
+console.log(fr[6]);
