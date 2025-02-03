7789504294374921c0ded814c08cafca16f864e8
ShahnamUlHassan <ibrahim2baig786@gmail.com>
2025-02-03 17:10:24 +0500

    Add files via upload

---
+++ Practice 12/script.js
@@ -0,0 +1,13 @@
+let name = "Shahnam";
+console.log(name.length);
+console.log(name[3]);
+
+let boy1 = "Shahnam";
+let boy2 = "Hassan";
+let sen = `${boy1} is a friend of ${boy2}`;
+console.log(sen);
+
+let fruit = "bana\'na'";
+console.log(fruit);
+
+
