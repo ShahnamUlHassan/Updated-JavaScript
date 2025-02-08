afd0744f6f4e76bd9621e2163e5bb9982c6d5528
ShahnamUlHassan <ibrahim2baig786@gmail.com>
2025-02-08 12:27:07 +0500

    Add files via upload

---
+++ Practice 16/script.js
@@ -0,0 +1,18 @@
+let num = [1 , 2 , 3 , 4];
+// let b = num.toString();
+// console.log(b);
+// let c = num.join("_");
+// console.log(c);
+// num.pop();
+// console.log(num);
+// let r = num.pop();
+// // console.log(num, r);
+// let r = num.push(54);
+// console.log(num, r);
+
+// let r = num.shift();
+// console.log(r, num);
+
+let r = num.unshift(76);
+console.log(r, num);
+
