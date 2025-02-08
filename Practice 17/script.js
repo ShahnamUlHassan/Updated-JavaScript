f6378ddc6d742ec760497798b817177aab39c9e8
ShahnamUlHassan <ibrahim2baig786@gmail.com>
2025-02-08 12:33:52 +0500

    Add files via upload

---
+++ Practice 17/script.js
@@ -0,0 +1,46 @@
+// let num = [1,2,3,4,5,6,7,8,9];
+// console.log(num.length);
+// delete num [0];
+// console.log(num);
+
+// let num = [1,2,3,4,5,6,7,8,9];
+// let num_more = [11,12,13,14,15,16,17,18,19];
+// let newArray = num.concat(num_more);
+// console.log(newArray);
+// console.log(num, num_more);
+
+// let num = [111,999,222,888,333,777,444,666,555];
+// num.sort();
+// console.log(num);
+
+// let compare =(a, b)=>{
+//     return a - b 
+// }
+// let num = [111,999,222,888,333,777,444,666,555];
+// num.sort(compare);
+// console.log(num);
+
+// let compare =(a, b)=>{
+//     return b - a 
+// }
+// let num = [111,999,222,888,333,777,444,666,555];
+// num.sort(compare);
+// console.log(num);
+
+// let compare =(a, b)=>{
+//     return b - a 
+// }
+// let num = [111,999,222,888,333,777,444,666,555];
+// num.sort(compare);
+// num.reverse()
+// console.log(num);
+
+// let num = [999,0,888,1,777,2,666,3,555,4,444,5,333,6,222,7,111,8];
+// num.splice(2,4,901,902,903);
+// let deletedvalues = num.splice(2,4,901,902,903);
+// console.log(num);
+// console.log(deletedvalues);
+
+let num = [999,0,888,1,777,2,666,3,555,4,444,5,333,6,222,7,111,8];
+num.slice(3);
+console.log(num);
