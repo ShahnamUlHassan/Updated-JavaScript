5f16472cba660f91b07af6f0575238937a57968e
ShahnamUlHassan <ibrahim2baig786@gmail.com>
2025-02-14 14:26:06 +0500

    Add files via upload

---
+++ Practice 26/script.js
@@ -0,0 +1,6 @@
+const changeBodyred = () => {
+    document.body.firstElementChild.style.background = "red"
+}
+let b = document.body
+console.log("First child of b is ",b.firstChild);
+console.log("Firs Element child of b is ",b.firstElementChild);
