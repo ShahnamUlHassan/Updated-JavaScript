0360320782e7edb5189d66df3193007e23a86f7a
ShahnamUlHassan <ibrahim2baig786@gmail.com>
2025-02-14 13:35:30 +0500

    Add files via upload

---
+++ Practice 23/script.js
@@ -0,0 +1,26 @@
+// let runAgain = true;
+// const canDrive = (age) => {
+//     return age >= 18 ? true:false
+// }
+// while (runAgain){
+//     let age = prompt("Enter your age")
+//     age = Number.parseInt(age)
+
+//     if(canDrive(age)){
+//         alert("yes you can drive")
+//     }else{
+//         alert("You cannot drive")
+//     }
+//     runAgain = confirm("Do you want to play again?")
+// }
+
+// let number = prompt("Enter your number");
+// number = Number.parseInt(number)
+// while(runAgain){
+//     if (number > 4){
+//         location.href = "https://google.com"
+//     }
+// }
+
+let color = prompt("enter the page background color");
+document.body.style.background = color
