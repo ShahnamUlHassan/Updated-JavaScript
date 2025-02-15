67baa3a5ea9f94b810a617274f9d5cb94da3957d
ShahnamUlHassan <ibrahim2baig786@gmail.com>
2025-02-15 16:00:22 +0500

    Add files via upload

---
+++ Practice 27/script.js
@@ -0,0 +1,5 @@
+let cardtitle = document.getElementsByClassName("card-title")[0];
+cardtitle.style.color = "red";
+
+let liveAlertPlaceholder = document.getElementById("liveAlertPlaceholder");
+liveAlertPlaceholder.style.color = "green";
