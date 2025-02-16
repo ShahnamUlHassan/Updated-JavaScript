aee37a6544849ebf9907f690b108cf7dcda3c78a
ShahnamUlHassan <ibrahim2baig786@gmail.com>
2025-02-16 12:48:55 +0500

    Add files via upload

---
+++ Practice 30/script.js
@@ -0,0 +1,20 @@
+let user = prompt("Enter S , W or G");
+let cpuI = Math.floor(Math.random() * 3);
+let cpu = ["S" , "W" , "G"][cpuI];
+
+const match = (cpu , user)=>{
+    if(cpu === user){
+        return "Nobody";
+    }
+    else if(cpu === "S" && user === "W"){
+        return cpu;
+    }
+    else if (cpu === "G" && user === "W"){
+        return cpu;
+    }
+    else if (cpu === "S" && user === "G"){
+        return user;
+    }
+}
+let result = match(cpu, user)
+document.write(`CPU:${cpu} and User:${user} \nThe winner is: ${result}`);
