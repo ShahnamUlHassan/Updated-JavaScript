282fbe5ce9d9b67828f149f57cc987b14c634ea2
ShahnamUlHassan <ibrahim2baig786@gmail.com>
2025-02-16 18:01:23 +0500

    Add files via upload

---
+++ Practice 31/scripts.js
@@ -0,0 +1,5 @@
+console.log(document.getElementsByTagName("span")[0]);
+console.dir(document.getElementsByTagName("span")[0]);
+console.log(document.body.firstElementChild.nodeName);
+console.log(document.body.firstChild.nodeName);
+console.log(document.body.textContent);
